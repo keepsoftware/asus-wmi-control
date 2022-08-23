1d1111b1f33249d172e89a4d5b81c14cdf8a708d
brq-apt <brq-apt@pbtech.net.br>
2022-08-23 20:55:05 -0300

    upgrade asus-wmi

--- prepare-for-current-kernel.sh
+++ prepare-for-current-kernel.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 VERSION=`uname -r | grep -o '^[0-9]\+\.[0-9]\+'`
+VERSION="5.18"
 
 if { echo $VERSION ; echo "5.7" ; } | sort -V -c 2>/dev/null
 then
@@ -13,5 +14,10 @@ echo "Using: $PATCHFILE"
 wget "https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/plain/drivers/platform/x86/asus-wmi.c?h=linux-$VERSION.y" -O 'asus-wmi.c'
 wget "https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/plain/drivers/platform/x86/asus-wmi.h?h=linux-$VERSION.y" -O 'asus-wmi.h'
 wget "https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/plain/drivers/platform/x86/asus-nb-wmi.c?h=linux-$VERSION.y" -O 'asus-nb-wmi.c'
-patch -p1 < $PATCHFILE
+
+
+wget "https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/plain/drivers/platform/x86/asus-wmi.c" -O 'asus-wmi.c'
+wget "https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/plain/drivers/platform/x86/asus-wmi.h" -O 'asus-wmi.h'
+wget "https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/plain/drivers/platform/x86/asus-nb-wmi.c" -O 'asus-nb-wmi.c'
+# patch -p1 < $PATCHFILE
 rm *.orig
